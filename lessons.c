56f6e72a2bdf1cc649158bddd98af58a01ecfd7a
Vladimír Hanhur <aqgz@email.cz>
2025-06-09 18:12:10 +0200

    update

--- lessons.c
+++ lessons.c
@@ -2,6 +2,13 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    int value = getchar();
+    int result = putchar(value);
+
+    char total = 'H';
+
+    puts("Hello, World!");
+    printf("%c\n %d\n", total, result);
     return 0;
 }
+// lesson 9
