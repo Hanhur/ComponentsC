b7876ec0835965193c7047eb68df3d13af453a74
Vladimír Hanhur <aqgz@email.cz>
2026-05-03 19:46:51 +0200

    update

---
+++ OutputDifferentTypesData/build.cmd
@@ -0,0 +1,5 @@
+
+gcc -Wall -E main.c -o main.i
+gcc -Wall -masm=intel -g3 -S main.i -o main.s
+gcc -Wall -g3 -c main.c
+gcc -Wall main.o -o main.exe
---
+++ OutputDifferentTypesData/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("String: ");
+	printf("Hello, World!!!\r\n");
+    printf("Char: ");
+	printf("%c\r\n", 131);
+    printf("Signed integer: ");
+	printf("%d\r\n", 3000000000);
+    printf("Unigned integer: ");
+	printf("%u\r\n", 3000000000);
+	printf("Signed short: ");
+	printf("%hd\r\n", 40000);
+	printf("Unsigned short: ");
+	printf("%hu\r\n", 40000);
+    printf("Signed long: ");
+	printf("%I64d\r\n", 18446744073709551615);
+	printf("Unsigned long: ");
+	printf("%llu\r\n", 18446744073709551615);
+    printf("Float: ");
+	printf("%f\r\n", 12.456f);
+	printf("Double: ");
+	printf("%lf\r\n", 12345.67891);
+    printf("Hex: ");
+	printf("0x%llX\r\n", 3000000000);
+    return 0;
+}
